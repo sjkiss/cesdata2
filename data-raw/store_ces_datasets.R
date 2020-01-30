cca734a088c5a5eeff96c2459096e00263ba0862
sjkiss <sjkiss@gmail.com>
2020-01-30 12:23:20 -0500

    Upload 65 and 68

--- data-raw/store_ces_datasets.R
+++ data-raw/store_ces_datasets.R
@@ -7,6 +7,8 @@ library(usethis)
 ##load data files
 #1993
 library(haven)
+ces65<-read_dta(file="~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/ces1965.dta")
+ces68<-read_dta(file="~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/ces1968.dta")
 ces72_jul<-read_sav(file="~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/ces1972/CES-E-1972-jun-july_F1.sav")
 ces72_sept<-read_sav(file="~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/ces1972/CES-E-1972-sept_F1.sav")
 ces72_nov<-read_sav(file="~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/ces1972/CES-E-1972-nov_F1.sav")
@@ -21,18 +23,18 @@ ces15phone<-read_sav(file='/Users/Simon/OneDrive - Wilfrid Laurier University/ca
 ces15web<-read_sav(file='/Users/Simon/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/CES15/web/CES15_CPS+PES_Web_SSI Full.SAV')
 
 #Combine 1972 files
-#Rename respondent idea variables for continuity
-names(ces72_jul)[1]
-names(ces72_sept)[1]
-names(ces72_nov)[1]
-ces72_jul %>%
-  rename("CASEID"="RES_ID1A") ->ces72_jul
-names(ces72_jul)[1]
+# #Rename respondent idea variables for continuity
+# names(ces72_jul)[1]
+# names(ces72_sept)[1]
+# names(ces72_nov)[1]
+# ces72_jul %>%
+#   rename("CASEID"="RES_ID1A") ->ces72_jul
+# names(ces72_jul)[1]
+#
+# names(ces72_sept)[1]
+# ces72_sept %>%
+#   rename("CASEID"="RESPID1A")->ces72_sept
+# names(ces72_sept)[1]
+# names(ces72_nov)[1]
 
-names(ces72_sept)[1]
-ces72_sept %>%
-  rename("CASEID"="RESPID1A")->ces72_sept
-names(ces72_sept)[1]
-names(ces72_nov)[1]
-
-use_data(ces72_jul, ces72_sept, ces72_nov,ces74, ces84, ces88,ces93,  overwrite=T)
+use_data(ces65, ces68,  overwrite=T)
