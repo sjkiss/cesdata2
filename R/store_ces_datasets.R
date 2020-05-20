c580db26c5bd324a97f6e634175bacba44247e38
sjkiss <sjkiss@gmail.com>
2020-05-20 10:24:23 -0400

    Renaming ces79 and ces74

---
+++ R/store_ces_datasets.R
@@ -0,0 +1,58 @@
+
+library(usethis)
+
+
+##load data files
+#1993
+library(haven)
+library(labelled)
+ces65<-read_dta(file="~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/ces1965.dta")
+ces68<-read_dta(file="~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/ces1968.dta")
+ces72_jul<-read_sav(file="~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/ces1972/CES-E-1972-jun-july_F1.sav")
+ces72_sept<-read_sav(file="~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/ces1972/CES-E-1972-sept_F1.sav")
+ces72_nov<-read_sav(file="~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/ces1972/CES-E-1972-nov_F1.sav")
+ces79<-read_sav(file="/Users/skiss/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/1974_1979_1980.sav")
+ces74<-read_sav(file="~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/CES-E-1974/CES-E-1974_F1.sav")
+ces84<-read_sav(file="~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/1984.sav")
+ces88<-read_sav(file="~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/CES1988.sav")
+ces93<-read_sav(file="~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/1993/CES-E-1993_F1.sav")
+ces97<-read_sav(file='~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/CES-E-1997/CES-E-1997_F1.sav')
+ces00<-read_sav(file='~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/CES-E-2000/CES-E-2000_F1.sav')
+ces0411<-read_sav(file='~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/ces.merged/CES_04060811_ISR_revised.sav')
+ces15phone<-read_sav(file='~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/CES15/CES2015-phone-release/CES2015_CPS-PES-MBS_complete.sav')
+ces15web<-read_sav(file='~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/CES15/web/CES15_CPS+PES_Web_SSI Full.SAV')
+ces19phone<-read_dta(file='~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/CES19/2019 Canadian Election Study - Phone Survey v1.0.dta', encoding="utf-8")
+ces19web<-read_dta(file='~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/CES19/2019 Canadian Election Study - Online Survey v1.0.dta', encoding="")
+
+library(labelled)
+library(tidyverse)
+look_for(ces19web, "occupation")
+ces19phone %>%
+  group_by(p52) %>%
+  summarise(n=n()) %>%
+  View()
+ces19web %>%
+  group_by(pes19_occ_text) %>%
+  summarise(n=n()) %>%
+View()
+
+#Combine 1972 files
+# #Rename respondent idea variables for continuity
+# names(ces72_jul)[1]
+# names(ces72_sept)[1]
+# names(ces72_nov)[1]
+# ces72_jul %>%
+#   rename("CASEID"="RES_ID1A") ->ces72_jul
+# names(ces72_jul)[1]
+#
+# names(ces72_sept)[1]
+# ces72_sept %>%
+#   rename("CASEID"="RESPID1A")->ces72_sept
+# names(ces72_sept)[1]
+# names(ces72_nov)[1]
+
+#use_data(ces65, ces68,  overwrite=T)
+use_data(ces74, overwrite=T)
+use_data(ces79, overwrite=T)
+use_data(ces19phone, overwrite=T)
+use_data(ces19web, overwrite = T)
