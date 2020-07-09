699f75fb37327a63be99456f203b5dcc7b33ccf2
sjkiss <sjkiss@gmail.com>
2020-07-09 16:16:40 -0400

    added NOC2

--- R/store_ces_datasets.R
+++ R/store_ces_datasets.R
@@ -25,34 +25,34 @@
 # ces19web<-read_dta(file='~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/CES19/2019 Canadian Election Study - Online Survey v1.0.dta', encoding="")
 
 #### Add Occupations to 2019 phone####
-data("ces19phone")
-library(readxl)
-noc<-read_excel(path="/Users/skiss/OneDrive - Wilfrid Laurier University/projects_folder/CES_Folder/Data/unique-occupations-updated.xls", col_names=T)
-head(noc)
-ces19phone$p52<-tolower(ces19phone$p52)
-library(tidyverse)
-noc %>%
-  select(p52, NOC) %>%
-  full_join(ces19phone, ., by="p52")->out
-out$NOC
-#Provide Check
-#Jobs uNique to Government program
-out %>%
-  filter(NOC==4168) %>%
-  select(p52)
-#Teachers
-out %>%
-  filter(NOC==4031) %>%
-  select(p52) %>%
-  print(n=100)
-#Carpenters
-out %>%
-  filter(NOC==7271) %>%
-  select(p52) %>%
-  print(n=100)
-#replace ces19phone with out
-ces19phone<-out
-names(ces19phone)
+# data("ces19phone")
+# library(readxl)
+# noc<-read_excel(path="/Users/skiss/OneDrive - Wilfrid Laurier University/projects_folder/CES_Folder/Data/unique-occupations-updated.xls", col_names=T)
+# head(noc)
+# ces19phone$p52<-tolower(ces19phone$p52)
+# library(tidyverse)
+# noc %>%
+#   select(p52, NOC) %>%
+#   full_join(ces19phone, ., by="p52")->out
+# out$NOC
+# #Provide Check
+# #Jobs uNique to Government program
+# out %>%
+#   filter(NOC==4168) %>%
+#   select(p52)
+# #Teachers
+# out %>%
+#   filter(NOC==4031) %>%
+#   select(p52) %>%
+#   print(n=100)
+# #Carpenters
+# out %>%
+#   filter(NOC==7271) %>%
+#   select(p52) %>%
+#   print(n=100)
+# #replace ces19phone with out
+# ces19phone<-out
+# names(ces19phone)
 # tail(names(ces0411))
 # library(labelled)
 # library(tidyverse)
@@ -76,5 +76,5 @@ names(ces19phone)
 #use_data(ces65, ces68, ces74, ces7980, ces84, ces88, ces93, ces97, ces00, ces0411, ces15phone, ces15web, ces19phone, ces19web, overwrite=T)
 #tail(names(ces7980))
 
-save(ces19phone, file="data/ces19phone.rda")
+#save(ces19phone, file="data/ces19phone.rda")
 
