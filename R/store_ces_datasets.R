10bb38abfa851d416ed712785ec352609692e835
Simon Kiss <skiss@Simons-MacBook-Pro.local>
2021-05-11 16:40:12 -0400

    reupload of data, including user_na=F

--- R/store_ces_datasets.R
+++ R/store_ces_datasets.R
@@ -5,54 +5,72 @@
 # ##load data files
 # #1993
 # library(haven)
-# library(labelled)
-# # ces65<-read_dta(file="~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/ces1965.dta")
-# # ces68<-read_dta(file="~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/ces1968.dta")
-# # ces72_jul<-read_sav(file="~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/ces1972/CES-E-1972-jun-july_F1.sav")
-# # ces72_sept<-read_sav(file="~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/ces1972/CES-E-1972-sept_F1.sav")
-# # ces72_nov<-read_sav(file="~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/ces1972/CES-E-1972-nov_F1.sav")
-# # ces74<-read_sav(file="~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/CES-E-1974/CES-E-1974_F1.sav")
-# # ces7980<-read_sav(file="/Users/skiss/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/1974_1979_1980.sav")
-# # ces84<-read_sav(file="~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/1984.sav")
-# # ces88<-read_sav(file="~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/CES1988.sav")
-# # ces93<-read_sav(file="~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/1993/CES-E-1993_F1.sav")
-# # ces97<-read_sav(file='~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/CES-E-1997/CES-E-1997_F1.sav')
-# # ces00<-read_sav(file='~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/CES-E-2000/CES-E-2000_F1.sav')
-# # ces0411<-read_sav(file='~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/ces.merged/CES_04060811_ISR_revised.sav')
-# #ces15phone<-read_sav(file='~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/CES15/CES2015-phone-release/CES2015_CPS-PES-MBS_complete.sav')
-# # ces15web<-read_sav(file='~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/CES15/web/CES15_CPS+PES_Web_SSI Full.SAV')
+# # library(labelled)
+# ces65<-read_dta(file="~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/ces1965.dta")
+# ces68<-read_dta(file="~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/ces1968.dta")
+# ces72_jul<-read_sav(file="~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/ces1972/CES-E-1972-jun-july_F1.sav")
+# ces72_sept<-read_sav(file="~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/ces1972/CES-E-1972-sept_F1.sav")
+# ces72_nov<-read_sav(file="~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/ces1972/CES-E-1972-nov_F1.sav")
+# ces74<-read_sav(file="~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/CES-E-1974/CES-E-1974_F1.sav")
+# ces7980<-read_sav(file="/Users/skiss/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/1974_1979_1980.sav")
+# ces84<-read_sav(file="~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/1984.sav")
+# ces88<-read_sav(file="~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/CES1988.sav")
+# ces93<-read_sav(file="~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/1993/CES-E-1993_F1.sav", user_na=F)
+# ces97<-read_sav(file='~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/CES-E-1997/CES-E-1997_F1.sav',user_na=F)
+# ces00<-read_sav(file='~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/CES-E-2000/CES-E-2000_F1.sav', user_na=F)
+# ces0411<-read_sav(file='~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/ces.merged/CES_04060811_ISR_revised.sav')
+# ces15phone<-read_sav(file='~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/CES15/CES2015-phone-release/CES2015_CPS-PES-MBS_complete.sav')
+# ces15web<-read_sav(file='~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/CES15/web/CES15_CPS+PES_Web_SSI Full.SAV')
 # ces19phone<-read_dta(file='~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/CES19/2019 Canadian Election Study - Phone Survey v1.0.dta', encoding="utf-8")
-# ces19web<-read_dta(file='~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/CES19/2019 Canadian Election Study - Online Survey v1.0.dta', encoding="")
+# ces19web<-read_dta(file='~/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/CES19/2019 Canadian Election Study - Online Survey v1.0.dta')
 # ces19_kiss<-read_dta(file='/Users/skiss/OneDrive - Wilfrid Laurier University/canadian_politics/canadian_election_studies/CES19/CES2019 Campaign Period Survey Kiss module data 2020-01-28.dta', encoding="")
-#
+# library(usethis)
+# data('ces19web')
+# library(labelled)
+# lookfor(ces19web, "federal")
+# View(var_label(ces19web))
+# val_labels(ces19web)
+# str(ces19web)
+# #rm(ces19web)
+# library(tidyverse)
+# ces19web %>%
+#   select(starts_with('cps19_spend')) %>%
+#   lookfor(., 'federal')
+# ncol(ces19web)
+# ces19web %>%
+#   select(275:310) %>%
+#   lookfor(., 'federal')
+# ?select
 # ####Combine 1972 files####
-# # #Rename respondent idea variables for continuity
-# # names(ces72_jul)[1]
-# # names(ces72_sept)[1]
-# # names(ces72_nov)[1]
-# # ces72_jul %>%
-# #   rename("CASEID"="RES_ID1A") ->ces72_jul
-# # names(ces72_jul)[1]
-# #
-# # names(ces72_sept)[1]
-# # ces72_sept %>%
-# #   rename("CASEID"="RESPID1A")->ces72_sept
-# # names(ces72_sept)[1]
-# # names(ces72_nov)[1]
+# #Rename respondent idea variables for continuity
+# names(ces72_jul)[1]
+# names(ces72_sept)[1]
+# names(ces72_nov)[1]
+# ces72_jul %>%
+#   rename("CASEID"="RES_ID1A") ->ces72_jul
+# names(ces72_jul)[1]
 #
-# #### Add Molly's Most Important Problem to 2019 Phone ####
+# names(ces72_sept)[1]
+# ces72_sept %>%
+#   rename("CASEID"="RESPID1A")->ces72_sept
+# names(ces72_sept)[1]
+# names(ces72_nov)[1]
+#
+# # #### Add Molly's Most Important Problem to 2019 Phone ####
 # library(readxl)
 # mip<-read_excel(path="/Users/skiss/OneDrive - Wilfrid Laurier University/projects_folder/CES_Folder/Data/mip.xlsx", col_names=T)
 #
 # library(stringr)
 # ces19phone$q7_lower<-str_to_lower(ces19phone$q7)
+# ces19phone$q7_lower
 # names(mip)
 # mip %>%
 #   select(q7_lower, q7_out) %>%
 #   full_join(ces19phone, ., by="q7_lower")->out
-#
+# names(out)
 # #use names in mip$mip15 for the numbers in CPS15_1.
 # #What I'm doing here is taking the same value labels from 2015 and applying them to 2019.
+#
 # mip %>%
 #   #I'm dumping out all the excess rows of the spreadsheet that do not have a value for CPS15_1
 #   #S'o I'm just keeping the rows that have the values and the value labels for the issues from 2015
@@ -61,9 +79,13 @@
 #   select(label=mip15, CPS15_1) %>%
 #   #This turns the CPS15_1 variable into a numeric variable
 #   mutate(value=as.numeric(CPS15_1))->mip_labels
+# mip_labels
 # #This takes the value labels in the value column and it makes them the names of the label variable
 # names(mip_labels$value)<-mip_labels$label
+# mip_labels$value
 # #This then turns out$mip into a labelled variable using the named mip_labels$value label that was justr created
+# names(out)
+# names(out)
 # out$q7_out<-labelled(out$q7_out, labels=mip_labels$value)
 #
 # #This performs a check
@@ -71,6 +93,7 @@
 #
 # #Overwrite ces19phone with out
 # ces19phone<-out
+#
 # #### Add Occupations to 2019 phone####
 # # data("ces19phone")
 #
@@ -104,18 +127,18 @@
 # ces19phone<-out
 # ces19phone[3059,'NOC']
 # names(ces19phone)
-#
-# library(labelled)
-# library(tidyverse)
-#
-#
-# # #### Save ces19phone after NOC and MIP###
-# names(out)
-#
-#
-#
-#
-#
+# #
+# # library(labelled)
+# # library(tidyverse)
+# #
+# #
+# # # #### Save ces19phone after NOC and MIP###
+# # names(out)
+# #
+# #
+# #
+# #
+# #
 # #### Add Occupations to 2019 Web####
 #
 # library(readxl)
@@ -178,10 +201,12 @@
 # #   select(-occ_text_joint, -noc_code) -> out
 # names(out)
 # ces19web<-out
-#
+
 #
 # #### Complete save command ###
-# #use_data(ces65, ces68, ces74, ces7980, ces84, ces88, ces93, ces97, ces00, ces0411, ces15phone, ces15web, ces19phone, ces19web, overwrite=T)
+# use_data(ces65, ces68, ces74, ces7980, ces84, ces88, ces93, ces97, ces00, ces0411, ces15phone, ces15web, ces19phone, ces19web,overwrite=T)
 # #tail(names(ces7980))
 # names(ces19phone)
-# use_data(ces19phone, ces19web, ces19_kiss,overwrite=T, version=3)
+
+
+
