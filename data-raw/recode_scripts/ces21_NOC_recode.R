792bd6e5bb255bd4032d08912dc8ce749b9696c6
Simon Kiss <sjkiss@gmail.com>
2024-01-15 13:25:16 -0500

    Caught occupation3 error

--- data-raw/recode_scripts/ces21_NOC_recode.R
+++ data-raw/recode_scripts/ces21_NOC_recode.R
@@ -11,5 +11,5 @@ read_excel(path=here("data-raw/2021_occupations_coded.xlsx"),
 #| label: ces21-lower-merge-with-noc
 ces21 %>%
   mutate(pes21_occ_text_lower=str_to_lower(pes21_occ_text)) %>%
-  left_join(., noc, join_by("pes21_occ_text_lower"=="title"))->ces21
+  left_join(., noc, by=c("pes21_occ_text_lower"="title"))->ces21
 
