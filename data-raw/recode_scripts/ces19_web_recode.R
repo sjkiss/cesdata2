15fff4e7eec1f1b644ae921e56c3c115d9698c72
Simon Kiss <sjkiss@gmail.com>
2024-02-23 13:46:06 -0500

    added income 2 to ces19web

--- data-raw/recode_scripts/ces19_web_recode.R
+++ data-raw/recode_scripts/ces19_web_recode.R
@@ -198,5 +198,27 @@ ces19web$sector<-Recode(ces19web$cps19_sector, "1=0; 4=0; 2=1; 5=NA")
 val_labels(ces19web$sector)<-c(`Private`=1, `Public`=0)
 ces19web$sector
 with(ces19web, table(cps19_sector, sector))
+
+#### Income
+lookfor(ces19web, "income")
+ces19web$cps19_income_number
+#Recode Income2 # Quintles
+ces19web$income2<-Recode(ces19web$cps19_income_number, "0:57500=1;
+57501:87500=2; 87501:125000=3;
+       125001:187500=4; 187501:9999999999=5; else=NA")
+
+val_labels(ces19web$income2)<-c(Lowest=1, Lower_Middle=2, Middle=3, Upper_Middle=4, Highest=5)
+ces19web$income2
+
+#$55,000 to $59,999
+#$85,000 to $89,999
+#$120,000 to $129,999
+#$175,000 to $199,999
+
+# Tertiles
+# $75,000 to $79,999
+# $130,000 to $139,999
+
+
 # Save the file
 save(ces19web, file=here("data/ces19web.rda"))
